7bc52fa01f406e6fc58da6f9f8a3ae62a1f9b28f
Ilya Prikhodko <zapaska96@gmail.com>
2025-03-02 20:42:21 +0500

    err practice

--- main.go
+++ main.go
@@ -437,8 +437,41 @@ func run() error {
 	}
 }
 
-func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
+// Sqrt with error
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func mySqrtWithError(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
 	}
+
+	z := 1.0
+
+	for i := 0; i < 10; i++ {
+		a := z*z - x
+		b := 2 * z
+		c := a / b
+
+		tempResult := z - c
+
+		if tempResult == z {
+			break
+		}
+
+		z = tempResult
+	}
+
+	return z, nil
+}
+
+func main() {
+	fmt.Println(mySqrtWithError(2))
+	fmt.Println(mySqrtWithError(-2))
+
+	fmt.Println(ErrNegativeSqrt(-2).Error())
 }
