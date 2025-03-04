1100b7165318fc649191cba47e2aa348eda7eaf5
Ilya Prikhodko <zapaska96@gmail.com>
2025-03-04 23:00:27 +0500

    бинарное дерево но лучше

--- binaryTree.go
+++ binaryTree.go
@@ -23,23 +23,51 @@ func Walk(t *tree.Tree, ch chan int) {
 
 }
 
+func CloseWalk(t *tree.Tree, ch chan int) {
+	Walk(t, ch)
+	close(ch)
+}
+
 func Same(t1, t2 *tree.Tree) bool {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
+	go CloseWalk(t1, ch1)
+	go CloseWalk(t2, ch2)
 
-	for i := 0; i < 10; i++ {
-		x1 := <-ch1
-		x2 := <-ch2
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
 
-		if x1 != x2 {
+		fmt.Println("vals", v1, v2, ok1, ok2)
+
+		if ok1 != ok2 {
+			fmt.Println("длина не та")
 			return false
 		}
+
+		if v1 != v2 {
+			fmt.Println("разные значения")
+			return false
+		}
+
+		if !ok1 && !ok2 {
+			fmt.Println("хз че это")
+			return true
+		}
+
 	}
 
-	return true
+	//for i := 0; i < 10; i++ {
+	//	x1 := <-ch1
+	//	x2 := <-ch2
+	//
+	//	if x1 != x2 {
+	//		return false
+	//	}
+	//}
+	//
+	//return true
 
 }
 
@@ -47,6 +75,7 @@ func main() {
 	t1 := tree.New(1)
 	t2 := tree.New(1)
 	t3 := tree.New(2)
+
 	fmt.Println(Same(t1, t2))
 	fmt.Println(Same(t1, t3))
 }
